e4ad0173b830771acfc3fb0bdd9ff41e86a2d0eb
GregHsr <gregoire.husser@etu.inp-n7.fr>
2023-10-15 16:04:36 +0200

    courbes

--- traitement.py
+++ traitement.py
@@ -57,17 +57,14 @@ for i in range(len(Angle_alpha)):
     Liste_resultante_portance.append(resultante(Liste_Portance[i]))
     Liste_resultante_trainee.append(resultante(Liste_Trainee[i]))
 
+# Rapport portance / traînée
+
+Liste_rapport_angle = []
+for i in range(len(Angle_alpha)):
+    Liste_rapport_angle.append(Liste_resultante_portance[i]/Liste_resultante_trainee[i])
+
 #print("Liste des résultantes de portance pour chaque angle :", Liste_resultante_portance)
 #print("Liste des résultantes de traînée pour chaque angle :", Liste_resultante_trainee)
-
-# Tracé du profil d'aile
-
-plt.figure(1)
-plt.plot(data_angle[1], data_angle[2],'o',label = "Profil d'aile")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.legend()
-plt.show()
 
 # Analyse pour différentes vitesses
 
@@ -88,5 +85,71 @@ for i in range(len(vitesse)):
     Liste_resultante_portance_vitesse.append(resultante(Liste_Portance_vitesse[i]))
     Liste_resultante_trainee_vitesse.append(resultante(Liste_Trainee_vitesse[i]))
 
+# Rapport portance / traînée
+
+Liste_rapport_vitesse = []
+for i in range(len(vitesse)):
+    Liste_rapport_vitesse.append(Liste_resultante_portance_vitesse[i]/Liste_resultante_trainee_vitesse[i])
+
+
 print("Liste des résultantes de portance pour chaque vitesse :", Liste_resultante_portance_vitesse)
 print("Liste des résultantes de traînée pour chaque vitesse :", Liste_resultante_trainee_vitesse)
+
+# Tracé du profil d'aile
+
+Liste_point_mesure = [0,2,3,4,5,7,11,15,18,27,28,29,30,31,33,34,36,38,42,49,55]
+plt.figure(1)
+plt.plot(data_angle[1], data_angle[2],'o',label = "Proints interpolés")
+plt.plot([data_angle[1][k] for k in Liste_point_mesure], [data_angle[2][k] for k in Liste_point_mesure],'o',label = "Points de mesure")
+v = plt.quiver(data_angle[1], data_angle[2], [np.cos(k) for k in data_vitesse[4]], [np.sin(k) for k in data_vitesse[4]],scale=0.05,scale_units='xy',headwidth=2)
+plt.quiverkey(v, 0.8, 0.75, 2, label=r"$t_l$", coordinates = "axes")
+plt.axis('equal')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+
+# Tracé de la portance et de la traînée en fonction de l'angle d'attaque
+
+plt.figure(2)
+plt.subplot(121)
+plt.plot(Angle_alpha, Liste_resultante_portance,'o',color = "orange",label = r"$F_z$")
+plt.xlabel(r"$\alpha$ [rad]")
+plt.legend()
+plt.ylabel("Portance [N/m]")
+plt.subplot(122)
+plt.plot(Angle_alpha, Liste_resultante_trainee,'o',color = "orange",label = r"$F_x$")
+plt.xlabel(r"$\alpha$ [rad]")
+plt.ylabel("Traînée [N/m]")
+plt.legend()
+
+# Tracé de la portance et de la traînée en fonction de la vitesse
+plt.figure(3)
+plt.subplot(121)
+plt.plot(vitesse, Liste_resultante_portance_vitesse,'o',color = "orange",label = r"$F_z$")
+plt.xlabel("Vitesse [m/s]")
+plt.legend()
+plt.ylabel("Portance [N/m]")
+plt.subplot(122)
+plt.plot(vitesse, Liste_resultante_trainee_vitesse,'o',color = "orange",label = r"$F_x$")
+plt.xlabel("Vitesse [m/s]")
+plt.ylabel("Traînée [N/m]")
+plt.legend()
+
+# Tracé du rapport portance / traînée en fonction de l'angle d'attaque
+plt.figure(4)
+plt.plot(Angle_alpha, Liste_rapport_angle,'o',color = "orange",label = r"$\frac{F_z}{F_x}$")
+plt.xlabel(r"$\alpha$ [rad]")
+plt.ylabel(r"$\frac{F_z}{F_x}$")
+plt.legend()
+
+# Tracé du rapport portance / traînée en fonction de la vitesse
+plt.figure(5)
+plt.plot(vitesse, Liste_rapport_vitesse,'o',color = "orange",label = r"$\frac{F_z}{F_x}$")
+plt.xlabel("Vitesse [m/s]")
+plt.ylabel(r"$\frac{F_z}{F_x}$")
+plt.legend()
+
+
+
+
+plt.show()
